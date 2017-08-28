6f2839672bdad7e2103018df45dfb3f0cfb3c928
coreyfedde <coreysfedde@gmail.com>
2017-08-28 17:21:44 -0400

    Adds virtuals
    - Added virtuals for monthly payment, total amount, total interest,
    as well as monthly interest and monthly principal.

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,12 +1,49 @@
+
 class Loan < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
 
 #reader_attr
-  def monthly_payment
-    self[:monthly_payment]
+  def principal
+    self[:principal].to_f
   end
+  def interest_rate
+    self[:interest_rate].to_f
+  end
+  def interest_rate_monthly
+    (interest_rate / 100) / 12
+  end
+  def loan_length
+    self[:loan_length].to_f
+  end
+  def loan_length_periods
+    loan_length / 12
+  end
+  def period
+    1
+  end
+
 # virtual attribute
   def monthly_payment
-    [loan_length]
+    if principal.zero? || interest_rate.zero?
+      0
+    else
+    # interest_rate
+    # (1 - (1 + interest_rate)**(-1))
+    # (principal * interest_rate) / (0)
+    (principal * interest_rate_monthly) / (1 - (1 + interest_rate_monthly)**(-loan_length))
+      # (principal * ((interest_rate / 100) / 12)) / (1 - (1 + ((interest_rate / 100) / 12))**(-1))
+    end
+  end
+  def monthly_interest
+    monthly_payment + (1 + interest_rate_monthly)**(period - 1) * (principal * interest_rate_monthly - monthly_payment)
+  end
+  def monthly_principal
+    monthly_payment - monthly_interest
+  end
+  def total_amount
+    monthly_payment * loan_length
+  end
+  def total_interest
+    (monthly_payment * loan_length) - principal
   end
 end
--- app/serializers/loan_serializer.rb
+++ app/serializers/loan_serializer.rb
@@ -1,12 +1,23 @@
 class LoanSerializer < ActiveModel::Serializer
   attributes :id, :name, :lender, :principal, :interest_rate, :loan_length,
-              :start_date, :current_monthly_payment, :monthly_payment
-
-  # def monthly_payment
-  #   return object.principal
-  #   # (object.principal * object.interest_rate) /
-  #   # (1 - (1 + object.interest_rate)^(-1))
-  # end
+              :start_date, :current_monthly_payment, :monthly_payment, :monthly_interest,
+              :monthly_principal, :interest_rate_monthly, :loan_length_periods, :period, :total_amount,
+              :total_interest
+            #   def principal
+            #     object.principal.to_f
+            #   end
+            #   def interest_rate
+            #     object.interest_rate.to_f
+            #   end
+            #
+            # # virtual attribute
+            #   def monthly_payment
+            #     binding.pry
+            #     # interest_rate
+            #     # (1 - (1 + interest_rate)**(-1))
+            #     # (principal * interest_rate) / (0)
+            #     ((principal * interest_rate) / ((1 - (1 + interest_rate)**(-1)).to_f)).to_f
+            #   end
 end
 
 #Loan
@@ -17,9 +28,9 @@ end
 # Current Payment (CP) <- Stretch
 
 # Implied
-# Minimum Monthly Payment (MP) = (P x R) / [1 - (1 + R)^(-n)
+# Minimum Monthly Payment (MP) = (P x R) / [1 - (1 + R)^(-L)
 # MP going toward interest (IMP) = MP + (1 + R)^(n-1) x (P x R - MP)
-# MP going toward principal (PMP) = MP - PMP
+# MP going toward principal (PMP) = MP - IMP
 # Total Amount over life of loan (Ta) = (MP x L)
 # Total Interest over life of loan (Ti) = (MP x L) - P
 # Period (N) = Period of the loan
